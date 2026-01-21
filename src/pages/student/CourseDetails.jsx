b297dad8d58532a179b8d4f25d86eac62e18f56e
Vishal Verma <vishalverma1514@gmail.com>
2026-01-21 23:03:48 +0530

    CoursesList completed

--- src/pages/student/CourseDetails.jsx
+++ src/pages/student/CourseDetails.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../../context/AppContext'
+import Loading from '../../components/student/Loading'
 
 const CourseDetails = () => {
-  return (
-    <div>CourseDetails</div>
-  )
+
+  const {id}= useParams()
+
+  const [courseData, setCourseData]=useState(null)
+
+  const {allCourses}=useContext(AppContext)
+
+  const fetchCourseData=async ()=>{
+    const findCourse=allCourses.find(course => course._id === id)
+    setCourseData(findCourse);
+  }
+  useEffect(()=>{
+    fetchCourseData()
+  },[])
+
+  return courseData ?  (
+    <>
+    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+      
+      <div className='absolute top-0 left-0 w-full h-96 -z-1 bg-linear-to-b from-cyan-100/70'></div>
+
+      {/* left Column */}
+      <div className='max-w-xl z-10 text-gray-500'>
+        <h1 className='md:text-3xl text-4xl  font-semibold text-gray-800'>{courseData.courseTitle}</h1>
+        <p className='pt-4 md:text-base text-sm' dangerouslySetInnerHTML={{__html : courseData.courseDescription.slice(0,200)}}></p>
+      </div>
+
+      {/* right Column */}
+      <div></div>
+    </div>
+    </>
+  ) : <Loading/>
 }
 
 export default CourseDetails
